2042e28a542741308e5431b075734c7a597d0b28
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-03-23 14:53:40 +0700

    AsideFiler

--- ShopeeClone/src/components/Input/Input.tsx
+++ ShopeeClone/src/components/Input/Input.tsx
@@ -1,26 +1,30 @@
+import { InputHTMLAttributes } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
-  placeholder?: string
-  className?: string
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  name: string
+  classNameInput?: string
+  classNameError?: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  register: UseFormRegister<any>
+  register?: UseFormRegister<any>
   rules?: RegisterOptions
 }
-export default function Input({ type, placeholder, className, errorMessage, name, register, rules }: Props) {
+export default function Input({
+  type,
+  placeholder,
+  className,
+  errorMessage,
+  name,
+  register,
+  rules,
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 focus:shadow-sm rounded-sm',
+  classNameError = 'mt-1 text-sm text-red-600 min-h-[1rem]'
+}: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
   return (
     <div className={className}>
-      <input
-        type={type}
-        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 focus:shadow-sm rounded-sm'
-        placeholder={placeholder}
-        autoComplete='on'
-        {...register(name, rules)}
-      />
-      <div className='mt-1 text-sm text-red-600 min-h-[1rem]'>{errorMessage}</div>
+      <input type={type} className={classNameInput} placeholder={placeholder} autoComplete='on' {...registerResult} />
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
--- ShopeeClone/src/pages/ProductLists/AsideFolder/AsideFilter.tsx
+++ ShopeeClone/src/pages/ProductLists/AsideFolder/AsideFilter.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
 import path from 'src/constants/path'
 
 export default function AsideFilter() {
   return (
     <div className='py-4'>
-      <Link to={path.home} className='flex items-center font-bold g'>
+      <Link to={path.home} className='flex items-center font-bold'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
@@ -21,6 +23,78 @@ export default function AsideFilter() {
         </svg>
         Tất cả danh mục
       </Link>
+      <div className='bg-gray-300 my-4'>
+        <ul>
+          <li className='py-2'>
+            <Link to={path.home} className='flex text-orange font-semibold'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z'
+                />
+                <path strokeLinecap='round' strokeLinejoin='round' d='M6 6h.008v.008H6V6Z' />
+              </svg>
+              Thời trang nam
+            </Link>
+          </li>
+          <li>
+            <Link to={path.home} className='flex px-6'>
+              Thời trang nam
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <Link to={path.home} className='flex items-center font-bold uppercase'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z'
+          />
+        </svg>
+        Bộ lọc tìm kiếm
+      </Link>
+      <div className='bg-gray-300 h-[1px] my-4' />
+      <div className='my-5'>
+        <div>Khoảng giá</div>
+        <form className='mt-2'>
+          <div className='flex items-center'>
+            <Input
+              type='text'
+              className='grow'
+              name='from'
+              placeholder='₫ Từ'
+              classNameInput='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            />
+            <span className='px-2'>-</span>
+            <Input
+              type='text'
+              className='grow'
+              name='from'
+              placeholder='₫ Đến'
+              classNameInput='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+            />
+          </div>
+          <Button type='submit' className='w-full p-2 uppercase bg-orange text-white text-sm hover:bg-orange'>
+            Áp dụng
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
